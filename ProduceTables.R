8a8c5389cd34c1cb0a0007e2956562d1101a2a18
BiostatsReportAutomation, Steve Ampah <BiostatsReportAutomation@gmail.com>
2020-05-26 19:05:11 -0500

    Update ProduceTables.R

--- ProduceTables.R
+++ ProduceTables.R
@@ -12,7 +12,7 @@ for(i in 1:length(x)){
 }
 
 #Bring in Data from github
-source("https://raw.githubusercontent.com/BiostatsReportAutomation/Table1_R_Excel_Output/master/baselinedata.csv")
+dat1=read_csv("https://raw.githubusercontent.com/BiostatsReportAutomation/Table1_R_Excel_Output/master/baselinedata.csv")
 
 
 #Bring in myTable1 function from github
